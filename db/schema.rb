dd60cbdeea06a3172f725af69fcdb7ab0d43b8d2
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-07 12:33:53 +0100

    create message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_020416) do
+ActiveRecord::Schema.define(version: 2021_07_07_113333) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "context"
+    t.integer "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "participations", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -38,6 +48,8 @@ ActiveRecord::Schema.define(version: 2021_07_07_020416) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "participations", "rooms"
   add_foreign_key "participations", "users"
 end
