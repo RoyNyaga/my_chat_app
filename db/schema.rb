6f8b36ca19ab72c142b11e9e88c566bda03cfcca
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-07 03:04:42 +0100

    create participation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_015045) do
+ActiveRecord::Schema.define(version: 2021_07_07_020416) do
+
+  create_table "participations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_participations_on_room_id"
+    t.index ["user_id"], name: "index_participations_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -29,4 +38,6 @@ ActiveRecord::Schema.define(version: 2021_07_07_015045) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "participations", "rooms"
+  add_foreign_key "participations", "users"
 end
